8ba38598fff013c70eb2091f9f97b39b59e96d53
unknown <ccrespo@cit014456.cit>
2016-11-16 16:25:54 -0200

    first commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This app uses React, Redux, React Router and other libraries
+                </p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
